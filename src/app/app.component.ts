01ef4c084679a8fd7a02320683ba97864d5526d2
pshubham8595 <pshubham8595@gmail.com>
2024-05-31 02:19:28 +0530

    Working Model of Angular JS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FileUploadService } from '../services/file-upload.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,61 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'door_locking_ui';
+
+  imageUrl: string | ArrayBuffer | null = "../assets/dummy-person.jpg";
+  imageSelected: boolean = false;
+  resultsFetched: boolean = false;
+  resultString: string = "Detected fault is : NA";
+  selectedFile:File | undefined;
+  inputElement: HTMLInputElement | null = null;
+  isUserValid:boolean = false;
+  predictionText: string = "NA"
+  userDetails: string = "NA"
+  showLoading:boolean = false;
+
+  constructor(public fileUploadService:FileUploadService){}
+  
+  onFileSelected(event: any): void {
+    const file: File = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.imageUrl = reader.result;
+        this.imageSelected = true;
+        this.selectedFile = file
+      };
+      reader.readAsDataURL(file);
+    } else {
+      this.imageUrl = null;
+      this.imageSelected = false;
+    }
+  }
+
+  sendFileToServer(){
+    console.log("Sending file to servier")
+    this.showLoading = true
+    this.resultsFetched = false
+    if (this.selectedFile) {
+      this.fileUploadService.uploadFile(this.selectedFile)
+        .subscribe(response => {
+          // Handle response if needed
+          let jsonData = JSON.parse(JSON.stringify(response))
+          console.log("Resp:" +JSON.stringify(response));
+          this.isUserValid = jsonData['isUserValid']
+          this.userDetails = jsonData['matchedUser']
+          if(this.isUserValid){
+            this.predictionText = "Selected image is of a valid user and matching with \n"+this.userDetails+" ";
+          }
+          else{
+            this.predictionText = "Selected image is of invalid user ";
+          }
+          
+          console.log(this.resultString);
+          this.resultsFetched = true;
+          this.showLoading = false;
+        }, error => {
+          console.error('Error uploading file:', error);
+        });
+    }
+  }    
 }
